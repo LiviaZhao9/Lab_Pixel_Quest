09a752cd9bd50741cb581a1e49e9b30ae0be04ab
LiviaZhao9 <liviazhao9@gmail.com>
2025-09-27 12:23:18 -0400

    Day 3 - Intro to C#

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEditor;
+using UnityEngine;
+
+public class GeoQuestCpm : MonoBehaviour
+{
+    // Start is called before the first frame update
+    string varOne = "Hello ";
+    void Start()
+    {
+        string varTwo = "World";
+        Debug.Log(varOne + varTwo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

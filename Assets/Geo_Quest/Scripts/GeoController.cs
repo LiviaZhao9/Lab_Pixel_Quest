42dc14cc5410d2bffab035e43828ddec9db7572c
LiviaZhao9 <liviazhao9@gmail.com>
2025-10-04 12:28:03 -0400

    Day 4 - Charecter movement intro

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -7,16 +7,56 @@ using UnityEngine;
 public class GeoQuestCpm : MonoBehaviour
 {
     // Start is called before the first frame update
-    string varOne = "Hello ";
+    private Rigidbody2D rb;
+    public int speed = 5;
+
     void Start()
     {
-        string varTwo = "World";
-        Debug.Log(varOne + varTwo);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+
+
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+
+
+
+        /*
+
+         if (Input.GetKeyDown(KeyCode.D))
+        {
+           rb.velocity = new Vector2(1, rb.velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-1, rb.velocity.y);
+        }
+
+        ------------------------------
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        */
+        private void onTriggerEnter2D(Collider2D collision)
+        {
+        Debug.Log("hit");
+        }
 }

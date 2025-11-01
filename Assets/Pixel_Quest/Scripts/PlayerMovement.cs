5071b84f6c4bc90c005f312e9558d3c1587a5df1
LiviaZhao9 <liviazhao9@gmail.com>
2025-11-01 12:23:22 -0400

    Day 7 - Player Movment

--- Assets/Pixel_Quest/Scripts/PlayerMovement.cs
+++ Assets/Pixel_Quest/Scripts/PlayerMovement.cs
@@ -7,15 +7,22 @@ public class PlayerMOvement : MonoBehaviour
     // Start is called before the first frame update
     private Rigidbody2D rb;
     public float speed = 5f;
+    private SpriteRenderer _spriteRenderer;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        _spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+       
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+        float xMovement = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xMovement * speed, rb.velocity.y);
+
+        if (xMovement > 0) { _spriteRenderer.flipX = true; }
+        else if (xMovement < 0) { _spriteRenderer.flipX = false;}
     }
 }

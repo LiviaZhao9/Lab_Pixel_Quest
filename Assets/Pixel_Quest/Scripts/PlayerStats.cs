1293c72b603fe103ccfb72b252eee0767db03273
LiviaZhao9 <liviazhao9@gmail.com>
2025-11-08 12:19:18 -0500

    Day 8 - Interactable Objects and Animation

--- Assets/Pixel_Quest/Scripts/PlayerStats.cs
+++ Assets/Pixel_Quest/Scripts/PlayerStats.cs
@@ -7,24 +7,54 @@ using UnityEngine.SceneManagement;
 public class PlayerStats : MonoBehaviour
 {
     // Start is called before the first frame update
-
-    public string nextLevel = "";
+    public Transform respawnPoint;
+    private int coinCounter = 0;
+    private int _health = 3;
+    private int _maxHealth = 3;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         switch (collision.tag)
         {
             case "Death":
                 {
-                    string thisLevel = SceneManager.GetActiveScene().name;
-                    SceneManager.LoadScene(thisLevel);
+                    _health--;
+                    if (_health <= 0)
+                        {
+                            string thisLevel = SceneManager.GetActiveScene().name;
+                            SceneManager.LoadScene(thisLevel);
+                            _health = _maxHealth;
+
+                        }
+                    else
+                        {
+                        transform.position = respawnPoint.position;
+  
+                    }
                     break;
                 }
-
+            case "Coin":
+                {
+                    coinCounter++;
+                    Destroy(collision.gameObject);
+                    break;
+                }
+            case "Health":
+                {
+                    if (_health < _maxHealth)
+                    {
+                        Destroy(collision.gameObject);
+                        _health++;
+                    }
+                    
+                    break;
+                }
             case "Finish":
                 {
+                    string nextLevel = collision.transform.GetComponent < LevelGoal > ().nextLevel;
                     SceneManager.LoadScene(nextLevel);
                     break;
                 }
+           
 
         }
 
